37adee7cb75bbae42b201daa55cdaa08437bd1ef
Sam <samuelakisanmi7@gmail.com>
2025-03-03 12:30:22 +0000

    added cart class

---
+++ S2 - CA2 - MVC Application/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace S2___CA2___MVC_Application.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int  Amount {  get; set; }
+
+        public decimal Total { get; set; }
+
+
+    }
+}
